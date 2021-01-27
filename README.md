1d8f0641cc7550424bc0a541f9b557e712c36f53
Eri Hanserkers <Eri.Hanserkers@afaforsakring.se>
2021-01-27 17:16:02 +0100

    Test med seedad data.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Load test using k6
 
 ## Run
-k6 run -e MY_HOSTNAME=ia.dev.ia.afaforsakring.se scripts/script.js
+k6 run -e MY_HOSTNAME=ia.dev.ia.afaforsakring.se scripts/LoginStartLogout.js
 k6 run -e MY_HOSTNAME=ia.dev.ia.afaforsakring.se scripts/HandelseLista.js
 
 ## Run with docker compose
 docker-compose up -d influxdb grafana  
-docker-compose run k6 run <test_params> scripts/<test_name>.js
+docker-compose run k6 run [options] scripts/<test_name>.js
 
 ### Grafana
 Dashboard at: http://localhost:3000/d/k6/k6-load-testing-results
--- scripts/HandelseLista.js
+++ scripts/HandelseLista.js
@@ -1,49 +1,64 @@
 import { sleep, check } from "k6";
 import http from "k6/http";
 
-const data = JSON.parse(open("OrganizationData.json"));
-
 const MAXUSERS = 20;
 
 export let options = {
-    stages: [
-      { duration: "10s", target: MAXUSERS },
-      { duration: "20s", target: MAXUSERS },
-      { duration: "10s", target: 0}
-    ], 
-    thresholds: {
-      'http_req_waiting': [ {threshold: 'avg<5000', abortOnFail: true }],
-      'http_req_duration': [{threshold: 'avg<1000', abortOnFail: false}]
-    }
+  setupTimeout: '10m',
+  stages: [
+    { duration: "10s", target: MAXUSERS },
+    { duration: "20s", target: MAXUSERS },
+    { duration: "10s", target: 0 }
+  ],
+  thresholds: {
+    'http_req_waiting': [{ threshold: 'avg<5000', abortOnFail: false }],
+    'http_req_duration': [{ threshold: 'avg<1000', abortOnFail: false }]
   }
+}
 
-export default function () {
-    let response;
-    let user = data[__VU % MAXUSERS];
+export function setup() {
+  let params = {
+    timeout: 600000
+  }
+  let response = http.post(`https://${__ENV.MY_HOSTNAME}/services/seedia/PerformanceTest/CreatePerformanceTestOrganisationJson?userCount=${MAXUSERS}`, '', params);
+  check(response, { 'Created': (r) => r.status === 201, });
+  let orgdata = JSON.parse(response.body);
+  return orgdata;
+}
 
-    response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Pages/Occurrences/HandelseLista.aspx?userId=${user.username}&userPwd=${user.password}`);
-    check(response, {'Status code': (r) => r.status === 200,});
+export function teardown(orgData) {
+  let response = http.del(`https://${__ENV.MY_HOSTNAME}/services/seedia/IAFtg/DeleteIAFtg?organizationId=${orgData[0].organisationId}`);
+  check(response, { 'Deleted': (r) => r.status === 204, });
+}
 
-    response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/News.html`);
-    check(response, {'Status code': (r) => r.status === 200,});
+export default function (orgData) {
+  let response;
 
-    response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/Favorites.html`);
-    check(response, {'Status code': (r) => r.status === 200,});
+  let user = orgData[__VU % MAXUSERS];
 
-    response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/Tips.html`);
-    check(response, {'Status code': (r) => r.status === 200,});
+  response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Pages/Occurrences/HandelseLista.aspx?userId=${user.username}&userPwd=${user.password}`);
+  check(response, { 'Status code': (r) => r.status === 200, });
 
-    response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/News`);
-    check(response, {'Status code': (r) => r.status === 200,});
+  response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/News.html`);
+  check(response, { 'Status code': (r) => r.status === 200, });
 
-    response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/Favorites`);
-    check(response, {'Status code': (r) => r.status === 200,});
+  response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/Favorites.html`);
+  check(response, { 'Status code': (r) => r.status === 200, });
 
-    response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/Tips`);
-    check(response, {'Status code': (r) => r.status === 200,});
+  response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/Tips.html`);
+  check(response, { 'Status code': (r) => r.status === 200, });
 
-    // Always returns status code 500. No need to check response.
-    response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Dashboard/IaNewsWidget/${user.userGuid}/sv-SE/${user.organisationGuid}/5`);
+  response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/News`);
+  check(response, { 'Status code': (r) => r.status === 200, });
 
-    sleep(1);
+  response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/Favorites`);
+  check(response, { 'Status code': (r) => r.status === 200, });
+
+  response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/Tips`);
+  check(response, { 'Status code': (r) => r.status === 200, });
+
+  // Always returns status code 500. No need to check response.
+  response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Dashboard/IaNewsWidget/${user.userGuid}/sv-SE/${user.organisationGuid}/5`);
+
+  sleep(1);
 }
