06850a4908b6b1fd9e63f6bbc2091af5f1d628d4
Emanuel Zere Goitom <Emanuel.ZereGoitom@afaforsakring.se>
2021-01-28 13:43:11 +0100

    Atgardlista parallelt

--- scripts/AtgardLista.js
+++ scripts/AtgardLista.js
@@ -3,14 +3,14 @@ import http from "k6/http";
 
 const data = JSON.parse(open("OrganizationData.json"));
 
-const MAXUSERS = 5;
+const MAXUSERS = 2;
 
 export let options = {
   setupTimeout: '10m',
   stages: [
+    { duration: "2s", target: MAXUSERS },
     { duration: "5s", target: MAXUSERS },
-    { duration: "20s", target: MAXUSERS },
-    { duration: "10s", target: 0}
+    { duration: "3s", target: 0}
   ]
 }
 
@@ -33,67 +33,20 @@ export default function main() {
         'Status code OK': (r) => r.status === 200,
       });      
 
-      response = http.get(
-        `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/News.html`
-      );
-      check(response, {
-        'Status code OK': (r) => r.status === 200,
-      });
-
-      response = http.get(
-        `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/Favorites.html`
-      );
-      check(response, {
-        'Status code OK': (r) => r.status === 200,
-      });
-
-      response = http.get(
-        `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/Tips.html`
-      );
-      check(response, {
-        'Status code OK': (r) => r.status === 200,
-      });
-
-      response = http.get(
-        `https://${__ENV.MY_HOSTNAME}/PreventionIA/signalr/negotiate?clientProtocol=2.1&connectionData=%5B%7B%22name%22%3A%22chathub%22%7D%5D&_=1611755543638`
-      );
-      check(response, {
-        'Status code OK': (r) => r.status === 200,
-      });
-
-      response = http.get(
-        `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/News`
-      );
-      check(response, {
-        'Status code OK': (r) => r.status === 200,
-      });
-
-      response = http.get(
-        `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/Favorites`
-      );
-      check(response, {
-        'Status code OK': (r) => r.status === 200,
-      });
-
-      response = http.get(
-        `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/Tips`
-      );
-      check(response, {
-        'Status code OK': (r) => r.status === 200,
-      });
-
-      response = http.get(
-        `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Dashboard/IaNewsWidget/${user.UserGuid}/sv-SE/${user.OrganisationGuid}/5`
-      );
-      check(response, {
-        'Status code OK': (r) => r.status === 200,
-      });
-
-      response = http.get(
-        `https://${__ENV.MY_HOSTNAME}/PreventionIA/signalr/start?transport=serverSentEvents&clientProtocol=2.1&connectionToken=Nc1wQrXRU4CZnyjG%2BAhgZX54SiQSUAj%2FaTGAghq7OI7RAe6eCZ5sHcE143pFPeMv%2Fxueq%2FL55xAlqJ%2F8Z52URObbL08MIIQSGGMqr%2ByK5hp4RxJtg8UQbfDt3hajiwil&connectionData=%5B%7B%22name%22%3A%22chathub%22%7D%5D&_=1611755543639`
-      );
-      check(response, {
-        'Status code OK': (r) => r.status === 200,
+      // Mulpitple requests running in parallell.
+      response = http.batch([
+        ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/News.html`, null, {}],
+        ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/Favorites.html`, null, {}],
+        ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/Tips.html`, null, {}],
+        ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/signalr/negotiate?clientProtocol=2.1&connectionData=%5B%7B%22name%22%3A%22chathub%22%7D%5D&_=1611755543638`, null, {}],
+        ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/News`, null, {}],
+        ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/Favorites`, null, {}],
+        ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/Tips`, null, {}],
+        ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Dashboard/IaNewsWidget/${user.userGuid}/sv-SE/${user.organisationGuid}/5`, null, {}], // Always fails (?)
+        ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/signalr/start?transport=serverSentEvents&clientProtocol=2.1&connectionToken=Nc1wQrXRU4CZnyjG%2BAhgZX54SiQSUAj%2FaTGAghq7OI7RAe6eCZ5sHcE143pFPeMv%2Fxueq%2FL55xAlqJ%2F8Z52URObbL08MIIQSGGMqr%2ByK5hp4RxJtg8UQbfDt3hajiwil&connectionData=%5B%7B%22name%22%3A%22chathub%22%7D%5D&_=1611755543639`, null, {}],
+      ]);
+      response.forEach(resp =>{
+        check(resp, {'Status code OK': (res) => res.status === 200});
       });
     }
   );
