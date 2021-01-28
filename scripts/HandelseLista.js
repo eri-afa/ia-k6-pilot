372354eca55ca5a6b6e5713fea3e57ea69c9b81f
Eri Hanserkers <Eri.Hanserkers@afaforsakring.se>
2021-01-28 09:03:43 +0100

    Error rate added.

--- scripts/HandelseLista.js
+++ scripts/HandelseLista.js
@@ -1,7 +1,10 @@
 import { sleep, check } from "k6";
 import http from "k6/http";
+import { Rate } from 'k6/metrics';
 
 const MAXUSERS = 20;
+
+let errorRate = new Rate('error_rate');
 
 export let options = {
   setupTimeout: '10m',
@@ -12,10 +15,13 @@ export let options = {
   ],
   thresholds: {
     'http_req_waiting': [{ threshold: 'avg<5000', abortOnFail: false }],
-    'http_req_duration': [{ threshold: 'avg<1000', abortOnFail: false }]
+    'http_req_duration': [{ threshold: 'avg<1000', abortOnFail: false }],
+    'error_rate': [{threshold: 'rate < 0.1', abortOnFail: true, delayAbortEval: '1m' }] // delayAbortEval is very useful! Look it up.
   }
 }
 
+// BEWARE, creating a performance test organization takes a lot of time, and this will be included
+// in the default metrics, thus affecting the test result.
 export function setup() {
   let params = {
     timeout: 600000
@@ -35,7 +41,8 @@ export default function (orgData) {
   let user = orgData[__VU % MAXUSERS];
 
   let response = http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Pages/Occurrences/HandelseLista.aspx?userId=${user.username}&userPwd=${user.password}`);
-  check(response, { 'HandelseLista status code': (r) => r.status === 200, });
+  const ok = check(response, { 'HandelseLista status code': (r) => r.status === 200, });
+  errorRate.add(!ok);
 
   // Mulpitple requests running in parallell.
   let responses = http.batch([
@@ -44,12 +51,13 @@ export default function (orgData) {
     ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/Sections/Views/Tips.html`, null, {}],
     ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/News`, null, {}],
     ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/Favorites`, null, {}],
-    ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/Tips`, null, {}]
+    ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Global/ResourceObjects/sv-SE/Tips`, null, {}],
+    ['GET', `https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Dashboard/IaNewsWidget/${user.userGuid}/sv-SE/${user.organisationGuid}/5`, null, {}], // Always fails (?)
   ]);
-  check(responses[0], {'Micellaneous resources status code': (res) => res.status === 200,});
-
-  // Always returns status code 500. No need to check response.
-  http.get(`https://${__ENV.MY_HOSTNAME}/PreventionIA/IA/api/Web/Dashboard/IaNewsWidget/${user.userGuid}/sv-SE/${user.organisationGuid}/5`);
-
+  responses.forEach(resp =>{
+    const ok = check(resp, {'Micellaneous resources status code': (res) => res.status === 200,});
+    errorRate.add(!ok);
+  });
+  
   sleep(1);
 }
